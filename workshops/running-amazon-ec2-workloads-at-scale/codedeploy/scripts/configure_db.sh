89fa1d2eaec1e0c9fa5960bb00d2984096ea284c
kurikei <kurikei@gmail.com>
2019-12-06 01:02:38 +0000

    edit configure_db.sh
    See:
    https://ec2spotworkshops.com/running-amazon-ec2-workloads-at-scale/deploy_app_codedeploy.html

--- workshops/running-amazon-ec2-workloads-at-scale/codedeploy/scripts/configure_db.sh
+++ workshops/running-amazon-ec2-workloads-at-scale/codedeploy/scripts/configure_db.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-sed -i 's/DB_HOST=127.0.0.1/DB_HOST=%endpoint%/g' /var/www/koel/.env
+sed -i 's/DB_HOST=127.0.0.1/DB_HOST=runningamazonec2workloadsatscale.cbrkibmmudmq.us-west-2.rds.amazonaws.com/g' /var/www/koel/.env
